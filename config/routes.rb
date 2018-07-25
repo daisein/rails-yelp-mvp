a509b8ce007f609d2fd4974ab1524fb16ae0dfab
Dai Li <dai.thomas.li3@gmail.com>
2018-07-25 16:15:57 +0800

    DDDD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    collection do
+      get "top", to: "restaurants#top"
+    end
+    member do
+      get "category", to: "restaurants#category"
+    end
+    member do
+      get "phone", to: "restaurants#phone"
+    end
+    resources :reviews, only: [:new, :create]
+  end
+
+    namespace :admin do
+    resources :restaurants, only: [:index]
+  end
+
+
 end
